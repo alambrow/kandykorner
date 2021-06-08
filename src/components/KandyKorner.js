4b922fb400641619b7853e1a65e1ab6dc3f51fe8
alambrow <alexander.lambrow@gmail.com>
2021-06-08 15:56:01 -0500

    ch 9 complete

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,12 +1,34 @@
 import React from "react"
-import { Route } from "react-router"
+import { Route, Redirect } from "react-router"
 import { ApplicationViews } from "../ApplicationViews"
 import { NavBar } from "./NavBar/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 export const KandyKorner = () => (
     <>
-        <h1>KandyKorner</h1>
-        <NavBar />
-        <ApplicationViews />
+        <Route
+            render={() => {
+                if (localStorage.getItem("kandy_customer")) {
+                    return (
+                        <>
+                        <h1>KandyKorner</h1>
+                        <NavBar />
+                        <ApplicationViews />
+                        </>
+                    )
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+            />
+
+            <Route path="/login">
+                <Login />
+            </Route>
+            <Route path="/register">
+                <Register />
+            </Route>
     </>
 )
+
